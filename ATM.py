f87a84046173f46b21924b426f9fb8a0ad89d57f
Mike <mikemej@hotmail.com>
2015-08-02 22:36:51 -0600

    File Name: ATM.py
    Modifying the ATM to updating the progress into the code.

--- ATM.py
+++ ATM.py
@@ -13,20 +13,17 @@
    Transfer_funds: Makes a transfers to the funds to another client of the bank.
 '''
 
-
 def user_language():
    global lang
    '''
       Provides the selected language, to translate the text according the selected language by the user
       The function returns:
       1- English
-      2- Español
+      2- Espanol
    '''
-   text="Please select your language / Por favor ingrese su idioma preferido: \n1- English\n2- Español\n"
+   text="\nWelcome to ATM Machine\n\nPlease select your language\n\n\nBienvenido al Cajero Automatico\n\nPor favor ingrese su idioma preferido: \n\n1- English\n2- Espanol\n"
    lang=input(text)
 
-   
-  
    
 def user_select():
    global lang
@@ -35,12 +32,13 @@ def user_select():
       The function consumes and integer and returns a menu with options to select
    '''
    if lang == 1:
-      text="Please select an option to continue:\n1- Account Balance\n2-Deposit Funds\n3-Transfer Funds\n4-WithDraw Funds\n"
+      text="\n\nPlease select an option to continue:\n1- Account Balance\n2-Deposit Funds\n3-Transfer Funds\n4-WithDraw Funds\n"
    else:
-      text="Por favor seleccione una opcion:\n1-Balance de cuenta\n2-Depostos\n3-Transferencias\n4-Retiros de Dinero\n"
+      text="\n\nPor favor seleccione una opcion:\n1-Balance de cuenta\n2-Depostos\n3-Transferencias\n4-Retiros de Dinero\n"
    option=input(text)
    
    return option
+
 
 #Client's database of the ATM machine 
 users_db={}
@@ -49,6 +47,15 @@ user_mike={'name':"Michael",'lastName':"Mejias",'userKey':12012,'user_id':11121,
 users_db={11121:user_mike}
 user_juan={'name':"Juan",'lastName':"Perez",'user_key':11011,'user_id':11131,'balance':25000}
 users_db[11131]=user_juan
+
+def account_balance(user_id):
+    global users_db
+    '''
+        The function retrieves the acoount balance of the user, and return the available funds for the client.
+    '''
+    b=1
+    b=users_db[user_id]['balance']
+    return b
 
 
 def user_validation(user_id):
@@ -60,7 +67,22 @@ def user_validation(user_id):
    result = False
    result = user_id in users_db
    return result   
+
+def insert_id():
+   global lang
+   '''
+      The functions returns the require user_id
+   '''
+   if lang == 1:
+      text="\nPlease insert you ID to continue...\n"
+   else:
+      text="\nPorfavor ingrese su identificacion...\n"
    
+   user_id=1
+   user_id=input(text)
+
+
+
 def welcome_screen():
    global lang
    lang=0
@@ -68,10 +90,25 @@ def welcome_screen():
    '''The welcome screen provides the minimal information to the ATM machine's user to select a next step. '''
    while lang == 0:
       user_language()
-      
-   option=user_select()
-   
+     
+   if user_validation(insert_id()):
+      option=user_select()
+      if option == 1:
+         balance=account_balance(user_id)    
+      elif option == 2:
+         balance=account_balance(user_id)              
+      elif option == 3:
+         balance=account_balance(user_id)
+      elif option == 4:
+         balance=account_balance(user_id)
+   else:
+      if lang == 1:
+         text="The User ID is not valide\nPlease confirm it and try again.\n"
+      else:
+         text="El usuario no es valido.\nPorfavor verifiquelo y trate de nuevo.\n"
+      print text
+      welcome_screen() 
    
    
 
-   
+welcome_screen() 
